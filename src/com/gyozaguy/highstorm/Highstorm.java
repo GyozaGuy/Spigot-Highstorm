8f3885b74280c8a3b8d8d679f9d567edd348add4
GyozaGuy <gyozaguy@gmail.com>
2015-06-30 22:12:16 -0600

    Actually added the Highstorm class

---
+++ src/com/gyozaguy/highstorm/Highstorm.java
@@ -0,0 +1,21 @@
+package com.gyozaguy.highstorm;
+
+public class Highstorm {
+	private int x;
+	private int y;
+	public Highstorm() {
+		// TODO - create storm
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+}
